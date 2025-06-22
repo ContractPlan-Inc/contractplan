45d2077981b321a3f3532e362ddb261bdda812ec
Nick Kirley <nick.kirley@gmail.com>
2025-06-21 21:09:07 -0400

    🔥 Remove backend CDK code to fix frontend build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body className="bg-white text-black">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }

cad3e22b4404bf3b307df5732c07d0b29dadccb4
Nick Kirley <nick.kirley@gmail.com>
2025-06-29 08:25:11 -0400

    ✅ Enable static export in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // disables Image Optimization for static export
+  },
 };
 
 module.exports = nextConfig;

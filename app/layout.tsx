983fccb8737029856875c627caa5a38a2a0f12ed
Nick Kirley <nick.kirley@gmail.com>
2025-07-07 12:06:39 -0400

    🧼 Ensure layout and Tailwind working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import './globals.css'
 
 export const metadata = {
   title: 'ContractPlan',
-  description: 'Empowering public teams with workflow automation.',
-};
+  description: 'Bring Order to Contract Chaos',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
+
